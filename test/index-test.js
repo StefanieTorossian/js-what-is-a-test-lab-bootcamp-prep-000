7516ecf4ab419c28c912e985922f7ce1dc2ad776
Stefanie Torossian <stefanietorossian@gmail.com>
2018-07-12 20:20:51 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Joe', () => {
-    it('returns "Susan"', () => {
+    it('equal "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
